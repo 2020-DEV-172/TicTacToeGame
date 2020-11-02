cd1704ce8c82bf76c831b713e2f51e50b1135e55
2020-Dev-072 <2020dev172@gmail.com>
2020-11-02 14:22:13 +0530

    Refactor: Extracted constants for reusability

--- src/main/java/com/bnpp/kata/TicTacToe.java
+++ src/main/java/com/bnpp/kata/TicTacToe.java
@@ -3,9 +3,10 @@ package com.bnpp.kata;
 public class TicTacToe {
 
     private char[][] boardGrid;
+    public static final Integer INDEX_3 = 3;
 
     public TicTacToe() {
-        this.boardGrid = new char[3][3];
+        this.boardGrid = new char[INDEX_3][INDEX_3];
     }
 
     public void playGame(int rowIndex, int colIndex, char player) {
