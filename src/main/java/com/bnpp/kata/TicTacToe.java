8ea54dd90ad4918432d4deb692e30610e1b7870c
2020-Dev-072 <2020dev172@gmail.com>
2020-11-02 14:17:53 +0530

    Refactor: Updated parameter names and extracted array initialization into constructor

--- src/main/java/com/bnpp/kata/TicTacToe.java
+++ src/main/java/com/bnpp/kata/TicTacToe.java
@@ -2,13 +2,17 @@ package com.bnpp.kata;
 
 public class TicTacToe {
 
-    private char[][] boardGrid = new char[3][3];
+    private char[][] boardGrid;
 
-    public void playGame(int i, int i1, char x) {
-        boardGrid[i][i1] = x;
+    public TicTacToe() {
+        this.boardGrid = new char[3][3];
     }
 
-    public char getPlayerAt(int i, int i1) {
-        return boardGrid[i][i1];
+    public void playGame(int rowIndex, int colIndex, char player) {
+        boardGrid[rowIndex][colIndex] = player;
+    }
+
+    public char getPlayerAt(int rowIndex, int colIndex) {
+        return boardGrid[rowIndex][colIndex];
     }
 }
