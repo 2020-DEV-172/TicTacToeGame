19494b2c1126fc805bd6f4e1467e3e3704f36537
2020-Dev-072 <2020dev172@gmail.com>
2020-11-02 14:51:49 +0530

    Player should change alternatively to mark the position on the board

--- src/main/java/com/bnpp/kata/TicTacToe.java
+++ src/main/java/com/bnpp/kata/TicTacToe.java
@@ -4,16 +4,26 @@ public class TicTacToe {
 
     private char[][] boardGrid;
     public static final Integer INDEX_3 = 3;
+    private Integer gameCounter = 0;
 
     public TicTacToe() {
         this.boardGrid = new char[INDEX_3][INDEX_3];
     }
 
-    public void playGame(int rowIndex, int colIndex, char player) {
-        boardGrid[rowIndex][colIndex] = player;
+    public void playGame(int rowIndex, int colIndex) {
+        gameCounter++;
+        boardGrid[rowIndex][colIndex] = getCurrentPlayer();
     }
 
     public char getPlayerAt(int rowIndex, int colIndex) {
         return boardGrid[rowIndex][colIndex];
+    }
+
+    public char getCurrentPlayer() {
+        return gameCounter % 2 == 0 ? 'O' : 'X';
+    }
+
+    public char getNextPlayer() {
+        return getCurrentPlayer() == 'X' ? 'O' : 'X';
     }
 }
--- src/test/java/com/bnpp/kata/TicTacToeTest.java
+++ src/test/java/com/bnpp/kata/TicTacToeTest.java
@@ -14,8 +14,18 @@ public class TicTacToeTest {
     public void playerShouldBeAbleToMarkXInAnyPositionAndRetrieveTheSame() {
         TicTacToe ticTacToe = new TicTacToe();
 
-        ticTacToe.playGame(INDEX_0, INDEX_0, X_PLAYER);
+        ticTacToe.playGame(INDEX_0, INDEX_0);
 
         assertThat(ticTacToe.getPlayerAt(INDEX_0, INDEX_0), is(X_PLAYER));
+    }
+
+    @Test
+    public void playerShouldChangeAlternatively() {
+        TicTacToe ticTacToe = new TicTacToe();
+
+        ticTacToe.playGame(INDEX_0, INDEX_0);
+
+        assertThat(ticTacToe.getCurrentPlayer(), is(X_PLAYER));
+        assertThat(ticTacToe.getNextPlayer(), is('O'));
     }
 }
