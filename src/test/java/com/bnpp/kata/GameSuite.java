0a8d57b1e80fe2296c6a8b5650c8a0e9cb0ad4af
2020-Dev-072 <2020dev172@gmail.com>
2020-11-03 00:27:07 +0530

    Added test class to the test suite

--- src/test/java/com/bnpp/kata/GameSuite.java
+++ src/test/java/com/bnpp/kata/GameSuite.java
@@ -4,6 +4,6 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({BoardTest.class, TicTacToeTest.class, PositionTest.class})
+@Suite.SuiteClasses({BoardTest.class, TicTacToeTest.class, PositionTest.class, GameStarterTest.class})
 public class GameSuite {
 }
