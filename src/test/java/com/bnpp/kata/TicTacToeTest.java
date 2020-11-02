1ef0322cd939243223904ac0f0bb0758809e8139
2020-Dev-072 <2020dev172@gmail.com>
2020-11-02 14:28:14 +0530

    Refactor: Extracted constants in test class for reusability

--- src/test/java/com/bnpp/kata/TicTacToeTest.java
+++ src/test/java/com/bnpp/kata/TicTacToeTest.java
@@ -7,12 +7,15 @@ import static org.junit.Assert.assertThat;
 
 public class TicTacToeTest {
 
+    public static final Integer INDEX_0 = 0;
+    public static final char X_PLAYER = 'X';
+
     @Test
     public void playerShouldBeAbleToMarkXInAnyPositionAndRetrieveTheSame() {
         TicTacToe ticTacToe = new TicTacToe();
 
-        ticTacToe.playGame(0, 0, 'X');
+        ticTacToe.playGame(INDEX_0, INDEX_0, X_PLAYER);
 
-        assertThat(ticTacToe.getPlayerAt(0, 0), is('X'));
+        assertThat(ticTacToe.getPlayerAt(INDEX_0, INDEX_0), is(X_PLAYER));
     }
 }
